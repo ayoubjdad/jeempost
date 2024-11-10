82dc4a33148d8348b0221727b411d109715a7ae0
Ayoub Jdad <ayoub.jdad@weexa.com>
2024-11-10 14:53:42 +0100

    fixes to deploy correctly

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,4 +1,4 @@
-export const serverUrl = "http://localhost:3000";
-// export const serverUrl = "https://jeempost.onrender.com";
+// export const serverUrl = "http://localhost:3000";
+export const serverUrl = "https://jeempost.onrender.com";
 
 export const newsUrl = `${serverUrl}/news`;
